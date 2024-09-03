1c86ab41a3c87c5575e5bce9ebc5bc3d741a7bfd
GHIFARI\USER <abudhar15@gmail.com>
2024-09-03 08:07:33 +0530

    first commit

---
+++ src/main/java/com/oops/_01Introduction.java
@@ -0,0 +1,14 @@
+package com.oops;
+
+public class _01Introduction {
+	/*
+	 *  Diff BetweenStructured & UN-Structured Pl's
+	 * 
+	 * 	1.Realtime Languanges -- Outdated Languages
+	 *  2.Follows Sturture    -- Follows Structures
+	 *  3.High Level Codes    -- Mnemonic Codes
+	 *  4.Flow of Exec is Good -- Flow of Excution is bad
+	 *  5.Using Functions features -- Dont have Functions/ Features
+	 * 
+	 */
+}
